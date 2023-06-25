274ec79ce21844e5bdf1a85c95d1c3665f032a71
sangweoncho <sangweon@nate.com>
2023-06-25 18:13:25 +0900

    230625 : 종료

--- Vuejs/vue30컴포넌트실습/comprj01/.eslintrc.js
+++ Vuejs/vue30컴포넌트실습/comprj01/.eslintrc.js
@@ -2,6 +2,12 @@ module.exports = {
   root: true,
   env: {
     node: true,
+
+    browser: true,
+    commonjs: true,
+    es6: true,
+    es2021: true,
+    jest: true,
   },
   extends: [
     'plugin:vue/vue3-essential',
@@ -11,9 +17,29 @@ module.exports = {
   parserOptions: {
     parser: '@babel/eslint-parser',
   },
+  plugins: ['prettier'],
+  ignorePatterns: ['node_modules/'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+
+    'prettier/prettier': [
+      'error',
+      { endOfLine: 'auto' },
+    ] /* 라인당 최대 글자 수 설정 */,
+    'linebreak-style': 'off',
+    'arrow-parens': ['error', 'always'] /* (a) => {} */,
+    'arrow-body-style': 'off' /* 화살표 함수 관련 설정 */,
+    'consistent-return': 'off' /* 화살표 함수 자동리턴 설정 */,
+    'func-names': 'off' /* 변수 선언시 var 사용하면 오류 발생 */,
+    'no-shadow': 'off',
+    'no-alert': 'off' /* 변수 선언시 var 사용하면 오류 발생 */,
+    'no-var': 'error' /* 변수 선언시 var 사용하면 오류 발생 */,
+    'no-unused-vars': 'off',
+    'no-param-reassign': [
+      'error',
+      { props: false },
+    ] /* 함수의 매개변수를 변경할 때 오류발생*/,
   },
   overrides: [
     {
---
+++ Vuejs/vue30컴포넌트실습/comprj01/src/components/todo/TodoList.vue
@@ -0,0 +1,236 @@
+<style scoped>
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: inline-block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+
+.closeModalBtn {
+  color: #62acde;
+}
+
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  width: 300px;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  color: #62acde;
+}
+
+.modal-body {
+  margin: 20px 0;
+}
+
+.modal-default-button {
+  float: right;
+}
+
+.modal-enter {
+  opacity: 0;
+}
+
+.modal-leave-active {
+  opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+
+li.checked {
+  background: #bbb;
+  color: #fff;
+  text-decoration: line-through;
+}
+
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+
+.list-enter-active,
+.list-leave-active {
+  transition: all 1s;
+}
+
+.list-enter,
+.list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+</style>
+
+<template>
+  <section>
+    <ul>
+      <!-- <li class="checked">
+        <i aria-hidden="true" class="checkBtn fas fa-check"></i>
+        주말 산책
+        <span type="button" class="removeBtn">
+          <i aria-hidden="true" class="far fa-trash-alt"></i>
+        </span>
+      </li> -->
+      <!-- "checked(todoItem.done)"  <==> "todoItem.done ? 'checked': null "  -->
+      <!-- v-on:click.stop="removeTodo(todoItem.id) -->
+      <li
+        v-for="todoItem in todoItems"
+        v-bind:key="todoItem.id"
+        v-bind:class="checked(todoItem.done)"
+        v-bind:data-id="todoItem.id"
+        v-on:click="(e) => doneToggle(e, todoItem.id)"
+      >
+        <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+        {{ todoItem.todo }}
+        <span
+          class="removeBtn"
+          type="button"
+          v-on:click="removeTodo(todoItem.id)"
+        >
+          <i class="far fa-trash-alt" aria-hidden="true"></i>
+        </span>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+// vuex 라이브러리에서 mapActions, mapMutations, mapState, mapGetters 함를 가져옵니다.
+// import { mapActions, mapMutations, mapState, mapGetters } from 'vuex';
+
+export default {
+  /* pdtmc^2w */
+  props: ['todoItems'],
+  data() {
+    /* 컴포넌트 안에서 사용되는 변수 등록. 개별 변수 */
+    return {};
+  },
+  //template: ``,
+  methods: {
+    checked(done) {
+      console.log(done);
+      return done ? 'checked' : null;
+    },
+    doneToggle(e, id) {
+      console.log(e.target, id);
+      // 부모 컴포넌트에게 doneToggle 이벤트 발생시킨다.
+      // const id = e.target.dataset.id;
+      debugger;
+      this.$emit('doneToggle', e, id);
+
+      const newarr = this.$data.todoItems.map((value, index, array) => {
+        console.log(value, index, array);
+        // debugger;
+        if (value.id === id) {
+          value.done = !value.done;
+          return value;
+        }
+        this.$data.todoItems = array;
+      });
+    },
+    removeTodo(id) {
+      debugger;
+      console.log(id);
+      // 부모 컴포넌트에게 doneToggle 이벤트 발생시킨다.
+      this.$emit('removeTodo', id);
+    },
+    /* 이벤트 핸들러 등록 + 일반 함수 */
+    /* vuex 를 사용하는 경우
+      mapActions 는 store의 actions 를 가져오는 헬퍼 메서드입니다.
+      namespaced: true를 설정한 경우 네임스페이스를 사용하기 때문에 store의 모듈 명을 적어주어야 합니다.
+      store 모듈에서 actions 를 가져오는 2가지 방식
+      1) store.모듈명.actions 이름 바꾸어 사용하기(추천방식)
+         ...mapActions('모듈명', { dispatch액션명1: '액션명1', dispatch액션명2: '액션명2' }),
+      2) store.모듈명.actions 이름 그대로 사용하기
+         ...mapActions('모듈명', ['액션명1', '액션명2']),
+      */
+  },
+  components: {
+    /* 전역 컴포넌트인 경우는 등록하지 않는다. 전역 컴포넌트는 프로토타입 체인으로 찾을 수 있기 때문에 */
+    /* 지역 컴포넌트나 파일 컴포넌트만 등록 한다. 예시) "태그명" : 컴포넌트명 */
+  },
+  computed: {
+    /* 자동처리 + 동기식. 메서드로 작성. return 필수. data 와 공존 불가 */
+    /* vuex 를 사용하는 경우
+      mapGetters 는 store의 getters 를 가져오는 헬퍼메서드입니다.
+      namespaced: true를 설정한 경우 네임스페이스를 사용하기 때문에 store의 모듈 명을 적어주어야 합니다.
+      store 모듈에서 getters 를 가져오는 2가지 방식
+      1) store.모듈명.getters 이름 바꾸어 사용하기
+         ...mapGetters('모듈명', { get게터명1: '게터명1', get게터명2: '게터명2' }),
+      2) store.모듈명.getters 이름 그대로 사용하기(추천방식)
+         ...mapGetters('모듈명', ['게터명1', '게터명2']),
+      */
+  },
+  watch: {
+    /* 자동처리 + 비동기식. data 에 등록된 프로퍼티(변수) 모니터링. 메서드로 작성. 매개변수 필수. 외부 api 호출을 위해서 사용 */
+  },
+  created() {
+    console.log('created');
+  },
+  mounted() {
+    console.log('mounted');
+    /* store의 actions 호출 */
+    // this.$store.dispatch('액션명', payload);
+  },
+  updated() {
+    console.log('updated');
+  },
+};
+</script>
