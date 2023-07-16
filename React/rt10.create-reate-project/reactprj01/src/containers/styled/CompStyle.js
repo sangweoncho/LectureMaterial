5912d379c8eaa9ddede024149c30d8f8d9c6af0d
sangweoncho <sangweon@nate.com>
2023-07-16 13:04:59 +0900

    react:230716

--- React/rt10.create-reate-project/reactprj01/src/containers/styled/CompStyle.js
+++ React/rt10.create-reate-project/reactprj01/src/containers/styled/CompStyle.js
@@ -1,11 +1,14 @@
-import React, { useState, useEffect, useRef, useCallback, useMemo, useReducer, Fragment, forwardRef, useImperativeHandle } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
-import { BrowserRouter, Routes, Route, NavLink, useParams, useLocation, useHistory, useNavigate } from 'react-router-dom';
 
 // 스타일을 위한 컴포넌트
 const StyledCompStyle = styled.div`
-  /* &로 자기 자신을 나타내고 삼항연산자, &&, || 문을 쓸수 있다
+  &:hover {
+    color: red; // <Thing> when hovered
+  }
+  /* &로 자기 자신(본인)을 나타내고 삼항연산자, &&, || 문을 쓸수 있다
+   * https://styled-components.com/docs/basics#adapting-based-on-props
    * 스타일 설정: https://styled-components.com/docs/basics#adapting-based-on-props
    * 스타일 상속: https://velog.io/@hwang-eunji/Styled-Components-리액트-스타일-컴포넌트
    */
@@ -20,14 +23,24 @@ const StyledCompStyle = styled.div`
 
 const StyledCircle = styled.div`
   /* &로 자기 자신을 나타내고 삼항연산자, &&,|| 문을 쓸수 있다
+   * https://styled-components.com/docs/basics#adapting-based-on-props
    * 스타일 설정: https://styled-components.com/docs/basics#adapting-based-on-props
    * 스타일 상속: https://velog.io/@hwang-eunji/Styled-Components-리액트-스타일-컴포넌트
    */
   width: 5rem;
   height: 5rem;
-  background: black;
+  /*background: black;*/
+  background: ${(props) => props.color || 'black'};
   border-radius: 50%;
   margin: auto;
+
+  /* 큰원 */
+  ${(props) =>
+    props.isHuge &&
+    css`
+      width: 10rem;
+      height: 10rem;
+    `}
 `;
 
 // const {...props} = props;
@@ -40,6 +53,10 @@ function CompStyle({ ...props }) {
       <hr />
 
       <StyledCircle></StyledCircle>
+      <hr />
+      <StyledCircle color={'blue'}></StyledCircle>
+      <hr />
+      <StyledCircle color={'red'} isHuge={true}></StyledCircle>
       <hr />
     </StyledCompStyle>
   );
